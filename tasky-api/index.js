7b05c219cb0a3baac152dc50560ecf01affe08a5
20107335 <20107335@mail.wit.ie>
2025-12-17 23:03:23 +0000

    Added Users Router

--- tasky-api/index.js
+++ tasky-api/index.js
@@ -1,31 +1,22 @@
 import dotenv from 'dotenv';
 import express from 'express';
-import tasksRouter from './api/tasks';
-import './db';
+import cors from 'cors';
+import tasksRouter from './api/tasks/index.js';
+import usersRouter from './api/users/index.js';
+import './db/index.js';
+
 
 dotenv.config();
 
-const errHandler = (err, req, res, next) => {
-  if(process.env.NODE_ENV === 'production') {
-    next(err);
-    return res.status(500).send(`Something went wrong!`);
-  }
-  res.status(500).send(`Hey!! You caught the error 👍👍. Here's the details: ${err.stack} `);
-};
-
-
 const app = express();
-
 const port = process.env.PORT || 8080;
 
-
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/tasks', tasksRouter);
+app.use('/api/users', usersRouter);
 
 app.listen(port, () => {
-  console.info(`Server running at ${port}`);
+  console.log(`Server running at ${port}`);
 });
-
-app.use(errHandler);
-
