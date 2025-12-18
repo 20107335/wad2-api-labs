80bd8bbc7cdb900a13ad7af26ba3887f17a75677
20107335 <20107335@mail.wit.ie>
2025-12-18 01:29:45 +0000

    Added tokens to task requests

--- tasky-react/src/api/tasky-api.jsx
+++ tasky-react/src/api/tasky-api.jsx
@@ -19,3 +19,58 @@ export const signup = async (username, password) => {
     });
     return response.json();
 };
+
+export const getTasks = async () => {
+    const response = await fetch(
+        `http://localhost:8080/api/tasks`, {
+            headers: {
+                'Authorization': window.localStorage.getItem('token')
+            }
+        }
+    )
+    return response.json();
+};
+
+export const addTask = async(data) => {
+    const res = await fetch(
+        `http://localhost:8080/api/tasks`,
+        {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': window.localStorage.getItem('token')
+            },
+            body: JSON.stringify(data)
+        }
+    )
+        return res.json();
+};
+
+export const deleteTask = async (id) => {
+    const res =  fetch(
+        `http://localhost:8080/api/tasks/${id}`,
+        {
+            method: 'DELETE',
+            headers: {
+                'Authorization': window.localStorage.getItem('token')
+            }
+        }
+    )
+    return res;
+};
+
+export const updateTask = async (data) => {
+    const res = await fetch(
+        `http://localhost:8080/api/tasks/${data._id}`,
+        {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': window.localStorage.getItem('token')
+            },
+            body: JSON.stringify(data)
+        }
+    )
+        return res.json();
+};
+
