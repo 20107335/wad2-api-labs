0309855c180a38d72a9eac4840a7fa596077658e
20107335 <20107335@mail.wit.ie>
2025-12-18 00:16:14 +0000

    Salt and Hash

--- tasky-api/api/users/userModel.js
+++ tasky-api/api/users/userModel.js
@@ -1,10 +1,32 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  username: { type: String, unique: true, required: true},
-  password: {type: String, required: true }
+  username: { type: String, unique: true, required: true },
+  password: { type: String, required: true }
 });
+
+// Compare password (login)
+UserSchema.methods.comparePassword = function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+// Find user by username
+UserSchema.statics.findByUserName = function (username) {
+  return this.findOne({ username });
+};
+
+UserSchema.pre('save', async function () {
+  // Only hash if password is new or changed
+  if (!this.isModified('password')) {
+    return;
+  }
+
+  const saltRounds = 10;
+  this.password = await bcrypt.hash(this.password, saltRounds);
+});
+
 
 export default mongoose.model('User', UserSchema);
